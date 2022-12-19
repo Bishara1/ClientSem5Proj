f2f4ea3ed24380d33d1fdb8023129180d6cfb62c
Tali1994 <talimuchnik12@gmail.com>
2022-12-19 14:19:09 +0200

    yyy

---
+++ Client/Client/gui_client/RemoteOrderController.java
@@ -0,0 +1,27 @@
+package gui_client;
+
+import java.awt.Button;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class RemoteOrderController {
+
+		@FXML
+		private ComboBox location;
+		@FXML
+		private Button backbtn;
+		@FXML
+		private Button startorderbtn ;
+		
+		
+		public void LocationCombo() {
+			
+		}
+		public void BackBtn() {
+			
+		}
+	    public void StartOrderBtn() {
+			
+		}
+	}
