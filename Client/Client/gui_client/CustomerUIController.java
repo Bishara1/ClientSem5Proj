1e8e434079c02c71c3fa82436c467a8c8f42f717
Tali1994 <talimuchnik12@gmail.com>
2022-12-28 12:34:45 +0300

    hghfhgff

--- Client/Client/gui_client/CustomerUIController.java
+++ Client/Client/gui_client/CustomerUIController.java
@@ -6,16 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-<<<<<<< HEAD
 import javafx.scene.control.Alert;
-=======
->>>>>>> branch 'master' of https://github.com/Bishara1/ClientSem5Proj
 import javafx.scene.control.Button;
-<<<<<<< HEAD
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
-=======
->>>>>>> branch 'master' of https://github.com/Bishara1/ClientSem5Proj
 import javafx.stage.Stage;
 
 public class CustomerUIController {
@@ -27,21 +21,14 @@ public class CustomerUIController {
 	private Button createremotebtn;
 	@FXML
 	private Button backbtn;
-	
-<<<<<<< HEAD
-	
+		
 	
 	public void SubReqBtn(ActionEvent event) throws Exception {
 		Alert alert = new Alert(AlertType.ERROR,"Your request has been recieved :)",ButtonType.OK);
 		alert.showAndWait();
-=======
-	public void SubReqBtn() {
-		
->>>>>>> branch 'master' of https://github.com/Bishara1/ClientSem5Proj
 	}
 	
 	public void CreateOrderBtn(ActionEvent event) throws Exception {
-<<<<<<< HEAD
 		((Node)event.getSource()).getScene().getWindow().hide();
 		Parent root = FXMLLoader.load(getClass().getResource("/gui_client/ekrutOrder.fxml"));
 		Stage primaryStage = new Stage();
@@ -49,18 +36,6 @@ public class CustomerUIController {
 		//scene.getStylesheets().add(getClass().getResource("/gui/loginsubscriber.css").toExternalForm());
 		primaryStage.setTitle("EKRUT");
 		primaryStage.setScene(scene);		
-=======
-		((Node)event.getSource()).getScene().getWindow().hide(); //hiding primary window
-		Stage primaryStage = new Stage();
-		
-		Parent root = FXMLLoader.load(getClass().getResource("/gui_client/ekrutOrder.fxml"));
-		Scene scene = new Scene(root);
-		
-		//scene.getStylesheets().add(getClass().getResource("/gui/.css").toExternalForm());
-		primaryStage.setTitle("Ekrut Order");
-		primaryStage.setScene(scene);
-		
->>>>>>> branch 'master' of https://github.com/Bishara1/ClientSem5Proj
 		primaryStage.show();
 		
 	}
