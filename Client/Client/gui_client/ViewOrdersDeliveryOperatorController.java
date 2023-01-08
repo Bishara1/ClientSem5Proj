e36044f712af5201801c6933e2490679d967d61c
Bishara1 <bish.150b@gmail.com>
2023-01-08 16:55:38 +0200

    My fucking masterpiece operator

--- Client/Client/gui_client/ViewOrdersDeliveryOperatorController.java
+++ Client/Client/gui_client/ViewOrdersDeliveryOperatorController.java
@@ -1,13 +1,15 @@
 package gui_client;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import client.ChatClient;
 import client.ClientUI;
 import common.Command;
 import common.Message;
-import gui_client.StockTableController.ViewItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,15 +19,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.shape.MeshView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import logic.Order;
-import logic.Subscriber;
 
 public class ViewOrdersDeliveryOperatorController implements Initializable {	
 	private ObservableList<Order> allOrders;
@@ -49,6 +53,9 @@ public class ViewOrdersDeliveryOperatorController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		GetRemoteOrders();
 		DisplayOrders();
+		
+		//allow status column to be editable
+		statusCol.setCellFactory(TextFieldTableCell.forTableColumn());
 	}
 	
 	private void GetRemoteOrders() {
@@ -77,5 +84,37 @@ public class ViewOrdersDeliveryOperatorController implements Initializable {
 		primaryStage.setScene(scene);
 		
 		primaryStage.show();
+	}
+	
+	@FXML
+	public void ChangeStatus(TableColumn.CellEditEvent<Order, String> productStringCellEditEvent) {
+		Order selectedOrder = OrdersTable.getSelectionModel().getSelectedItem();
+		int index = OrdersTable.getSelectionModel().getSelectedIndex();
+		if (productStringCellEditEvent.getNewValue().equals("Delivering") ||
+				productStringCellEditEvent.getNewValue().equals("delivering")) {
+			
+			selectedOrder.setOrder_status(productStringCellEditEvent.getNewValue());
+			allOrders.set(index, selectedOrder);
+		}
+		
+		else {
+			Alert alert = new Alert(AlertType.CONFIRMATION,"Invalid Status! Status should be \""
+					+ "Pending\" or \"Delivering\".",ButtonType.OK);
+			alert.showAndWait();
+			
+		}
+	}
+	
+	public void UpdateOrders() {
+		ArrayList<Order> updateOrders = ConvertToArrayList();
+		ClientUI.chat.accept(new Message(updateOrders, Command.InsertOrder));
+	}
+	
+	private ArrayList<Order> ConvertToArrayList() {
+		List<Order> updatedOrdersList = allOrders.stream().collect(Collectors.toList());
+		ArrayList<Order> updatedOrders = new ArrayList<>(updatedOrdersList);
+		
+		System.out.println(updatedOrders.get(0));
+		return updatedOrders;
 	}
 }
